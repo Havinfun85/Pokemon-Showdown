5b3eeaa21984d37dc513b7cdb9f009142ad85533
Scizornician <Arranswim@gmail.com>
2013-04-06 21:48:51 +0200

    Added to Voice description

--- chat-commands.js
+++ chat-commands.js
@@ -1678,11 +1678,11 @@ function parseCommandLocal(user, cmd, target, room, socket, message) {
 		showOrBroadcastStart(user, cmd, room, socket, message);
 		showOrBroadcast(user, cmd, room, socket,
 			'<div class="infobox">' +
-			'+ <b>Voice</b> - They can use ! commands like !groups, and talk during moderated chat<br />' +
+			'+ <b>Voice</b> - They can use !commands and manage tours, and talk during moderated chat<br />' +
 			'% <b>Driver</b> - The above, and they can also mute users and run tournaments<br />' +
 			'@ <b>Moderator</b> - The above, and they can ban users and check for alts<br />' +
 			'&amp; <b>Leader</b> - The above, and they can promote moderators and force ties<br />'+
-			'~ <b>Administrator</b> - They can do anything, like change what this message says'+
+			'~ <b>Administrator</b> - They can do anything, Come at me bro!'+
 			'</div>');
 		return false;
 		break;
